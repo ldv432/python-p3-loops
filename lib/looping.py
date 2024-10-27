b5c7deef698b170ddaf518414bbaa87fef44e22d
ldv432 <ldv432@gmail.com>
2024-10-27 12:37:09 -0600

    FIZZ BUZZED

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
-def happy_new_year():
-    # code goes here!
-    pass
+def happy_new_year(num=10):
+    while num >=1:
+        print(num)
+        num -= 1
+    print("Happy New Year!")
+
+happy_new_year()
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num*num for num in int_list]
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range (1,101):
+        if num%3 == 0 and num%5==0:
+            print("FizzBuzz")
+        elif num%3 == 0:
+            print("Fizz")
+        elif num%5==0:
+            print("Buzz")
+        else:
+            print(num)
+
+    
